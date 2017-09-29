abaf53fcab45ae9f43bd57d31ba218c3761daccb
Jabar Asadi <jabbar.asadi@bamilo.com>
2017-09-29 17:51:17 +0330

    Update Settings.php

--- src/PushNotification/Settings.php
+++ src/PushNotification/Settings.php
@@ -5,13 +5,13 @@ namespace PushNotification;
 interface Settings
 {
     /** string the path to you apn certification file  */
-    const APNS_CERTIFICATION_FILE_PATH = __DIR__ . '/../../pushcert.pem';
+    const APNS_CERTIFICATION_FILE_PATH = '';
 
     /** string apn endpoint url */
     const APNS_URL = 'ssl://gateway.push.apple.com:2195';
 
     /** string fcm api key */
-    const FCM_API_ACCESS_KEY = 'AIzaSyCvYak-qzV4aeOZmHuHmIGmyojOmpjJJd8';
+    const FCM_API_ACCESS_KEY = '';
 
     /** string fcm endpoint url */
     const FCM_URL = 'https://fcm.googleapis.com/fcm/send';
@@ -19,6 +19,7 @@ interface Settings
     /** string google content type */
     const GOOGLE_CONTENT_TYPE = 'application/json';
 
+    /** string socket open limitation*/
     const Socket_Time_To_Live = 60;
 
 }
